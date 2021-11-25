2e3ad14ef575e94a7ffe7a5f49182f44c38a1bdd
Juicy220 <95038919+Juicy220@users.noreply.github.com>
2021-11-25 18:02:55 +0300

    Add files via upload

---
+++ Номера простые с шагом 1/WindowsApp4/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ColumnHeadersVisible = False
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 38)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.Size = New System.Drawing.Size(676, 529)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(280, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(700, 579)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Номера простые с шагом 1/WindowsApp4/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.ColumnCount = 101
+        Dim i = 1
+        Do While i < 101
+            DataGridView1.Rows.Add()
+            i += 1
+        Loop
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Red
+        DataGridView1.RowsDefaultCellStyle.BackColor = Color.Red
+        Dim num As Integer = 2
+        Dim row As Integer = 50
+        Dim col As Integer = 50
+        'For a As Integer = 7 To 8
+        '    DataGridView1.Rows(row).Cells(a).Value = num
+        '    num += 1
+        '    col = a
+        'Next
+        Dim b As Integer
+        Dim j As Integer
+        Dim counta As Integer = 0
+        Dim countb As Integer = 1
+        While b < 50
+            ' DataGridView1.Rows(row).Cells(col).Value = num
+            'num += 1
+            col += 1
+            For a As Integer = 0 To counta
+                'DataGridView1.Rows(row).Cells(col).Value = num
+                DataGridView1.Rows(row).Height = 5
+                DataGridView1.Columns(col).Width = 5
+                For j = 2 To (num - 1)
+                    If num Mod j = 0 Then
+                        DataGridView1.Rows(row).Cells(col).Style.BackColor = Color.White
+                    End If
+                Next
+                num += 1
+                row -= 1
+            Next
+            For a As Integer = 0 To 0
+                ' DataGridView1.Rows(row).Cells(col).Value = num
+                DataGridView1.Rows(row).Height = 5
+                DataGridView1.Columns(col).Width = 5
+                For j = 2 To (num - 1)
+                    If num Mod j = 0 Then
+                        DataGridView1.Rows(row).Cells(col).Style.BackColor = Color.White
+                    End If
+                Next
+                num += 1
+            Next
+
+            For a As Integer = 0 To countb
+                col -= 1
+                ' DataGridView1.Rows(row).Cells(col).Value = num
+                DataGridView1.Rows(row).Height = 5
+                DataGridView1.Columns(col).Width = 5
+                For j = 2 To (num - 1)
+                    If num Mod j = 0 Then
+                        DataGridView1.Rows(row).Cells(col).Style.BackColor = Color.White
+                    End If
+                Next
+                num += 1
+            Next
+            For a As Integer = 0 To countb
+                row += 1
+                'DataGridView1.Rows(row).Cells(col).Value = num
+                DataGridView1.Rows(row).Height = 5
+                DataGridView1.Columns(col).Width = 5
+                For j = 2 To (num - 1)
+                    If num Mod j = 0 Then
+                        DataGridView1.Rows(row).Cells(col).Style.BackColor = Color.White
+                    End If
+                Next
+                num += 1
+            Next
+            For a As Integer = 0 To countb
+                col += 1
+                ' DataGridView1.Rows(row).Cells(col).Value = num
+                DataGridView1.Rows(row).Height = 5
+                DataGridView1.Columns(col).Width = 5
+                For j = 2 To (num - 1)
+                    If num Mod j = 0 Then
+                        DataGridView1.Rows(row).Cells(col).Style.BackColor = Color.White
+                    End If
+                Next
+                num += 1
+            Next
+            'col += 1
+            b += 1
+            counta += 2
+            countb += 2
+        End While
+        DataGridView1.Rows(50).Cells(50).Style.BackColor = Color.Black
+    End Sub
+End Class
